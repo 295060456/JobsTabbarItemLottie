5d0c85bbce8765700b7e6d0bc37b496dd46cd869
Jobs <lg295060456@gmail.com>
2020-11-03 13:45:14 +0700

    no message

--- 外部挂载的VC，用户自定义/VC@9.m
+++ 外部挂载的VC，用户自定义/VC@9.m
@@ -14,11 +14,29 @@
 
 @implementation VC_9
 
+- (void)dealloc {
+    NSLog(@"Running self.class = %@;NSStringFromSelector(_cmd) = '%@';__FUNCTION__ = %s", self.class, NSStringFromSelector(_cmd),__FUNCTION__);
+    PrintRetainCount(self);
+}
+
+- (instancetype)init{
+    if (self = [super init]) {
+//        @weakify(self)
+        PrintRetainCount(self);
+    }return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = RandomColor;
     
     self.isHiddenNavigationBar = NO;//这一句决定是否有navigationbar
+    PrintRetainCount(self);
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    PrintRetainCount(self);
 }
 
 
