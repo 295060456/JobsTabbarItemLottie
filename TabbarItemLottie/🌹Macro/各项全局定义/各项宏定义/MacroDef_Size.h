14a00e212f5befbbc6fb1b213a6d60100d5f954e
Jobs <lg295060456@gmail.com>
2020-12-16 21:36:04 +0700

    no message

--- TabbarItemLottie/🌹Macro/各项全局定义/各项宏定义/MacroDef_Size.h
+++ TabbarItemLottie/🌹Macro/各项全局定义/各项宏定义/MacroDef_Size.h
@@ -8,6 +8,17 @@
 #ifndef MacroDef_Size_h
 #define MacroDef_Size_h
 
+// 屏幕大小、宽、高
+#ifndef SCREEN_BOUNDS
+#define SCREEN_BOUNDS [UIScreen mainScreen].bounds
+#endif
+#ifndef SCREEN_WIDTH
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#endif
+#ifndef SCREEN_HEIGHT
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#endif
+
 #define MianScreen          UIScreen.mainScreen.bounds.size
 #define Device_Width        MianScreen.width//获取屏幕宽高
 #define Device_Height       MianScreen.height
---
+++ TabbarItemLottie/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
@@ -0,0 +1,54 @@
+//
+//  Manual_Add_ThirdParty.h
+//  TabbarItemLottie
+//
+//  Created by Jobs on 2020/12/16.
+//
+
+#ifndef Manual_Add_ThirdParty_h
+#define Manual_Add_ThirdParty_h
+
+#pragma mark —— BaseCustomizeUIKit
+/// NSObject
+#import "NSObject+Sound.h"
+#import "NSObject+Extras.h"
+/// UIView
+#import "UIView+Measure.h"
+#import "UIView+Extras.h"
+#import "UIView+Animation.h"
+#import "UIView+SuspendView.h"
+/// UIButton
+#import "UIButton+ImageTitleSpacing.h"
+/// UIImage
+#import "LoadingImage.h"
+/// UIViewController
+#import "UIViewController+BaseVC.h"
+#import "UIViewController+BackBtn.h"
+#import "UIViewController+BRPickerView.h"
+#import "UIViewController+BWShareView.h"
+#import "UIViewController+GifImageView.h"
+#import "UIViewController+JPImageresizerView.h"
+#import "UIViewController+JXCategoryListContentViewDelegate.h"
+#import "UIViewController+JXPagerViewListViewDelegate.h"
+#import "UIViewController+MJRefresh.h"
+#import "UIViewController+Shake.h"
+#import "UIViewController+TZImagePickerController.h"
+#import "UIViewController+TZImagePickerControllerDelegate.h"
+#import "UIViewController+TZLocationManager.h"
+#import "UIViewController+NavigationBar.h"
+/// UINavigationController
+#import "BaseNavigationVC.h"
+/// UINavigationBar
+#import "NavigationBar.h"
+/// NSObject
+#import "NSObject+Shake.h"
+
+#pragma mark —— Manual_Add_ThirdParty_h
+#import "BWShareView.h"// 分享控件
+#import "TabbarControllerSysEx.h"// 深度拓展系统UITabBar全家桶
+#import "ECAuthorizationTools.h"
+#import "TimerManager.h"
+#import "SuspendBtn.h"
+#import "ECAuthorizationTools.h"
+
+#endif /* Manual_Add_ThirdParty_h */
