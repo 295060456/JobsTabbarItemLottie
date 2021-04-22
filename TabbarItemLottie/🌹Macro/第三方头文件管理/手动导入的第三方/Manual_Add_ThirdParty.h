f74c741273e510656944fc19e79a682e900fbc17
Jobs <lg295060456@gmail.com>
2021-04-22 10:38:06 +0700

    no message

--- TabbarItemLottie/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
+++ TabbarItemLottie/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
@@ -8,61 +8,7 @@
 #ifndef Manual_Add_ThirdParty_h
 #define Manual_Add_ThirdParty_h
 
-/// BaseCustomizeUIKit
-#pragma mark —— Log
-#import "DebugLogDescription.h"
-#pragma mark —— NSObject
-#import "NSObject+Sound.h"
-#import "NSObject+Extras.h"
-#pragma mark —— UIView
-/// BaseView
-#import "BaseView.h"
-/// UIView+Category
-#import "UIView+Measure.h"
-#import "UIView+Extras.h"
-#import "UIView+Animation.h"
-#import "UIView+SuspendView.h"
-#import "UIView+EmptyData.h"
-#import "UIView+Gesture.h"
-#import "UIView+MJRefresh.h"
-#import "UIView+ViewController.h"
-#pragma mark —— UIButton
-#import "UIButton+ImageTitleSpacing.h"
-#pragma mark —— UIImage
-#import "LoadingImage.h"
-#import "UIImage+Extras.h"
-#pragma mark —— UIViewController
-#import "UIViewController+BaseVC.h"
-#import "UIViewController+BackBtn.h"
-#import "UIViewController+BRPickerView.h"
-#import "UIViewController+BWShareView.h"
-#import "UIViewController+GifImageView.h"
-#import "UIViewController+JPImageresizerView.h"
-#import "UIViewController+JXCategoryListContentViewDelegate.h"
-#import "UIViewController+JXPagerViewListViewDelegate.h"
-#import "UIViewController+MJRefresh.h"
-#import "UIViewController+Shake.h"
-#import "UIViewController+BaseNavigationBar.h"
-#import "UIViewController+EmptyData.h"
-#import "UIViewController+XLBubbleTransition.h"
-#pragma mark —— UINavigationController
-#import "BaseNavigationVC.h"
-#pragma mark —— UINavigationBar
-#import "BaseNavigationBar.h"
-#pragma mark —— UITextView
-/// UITextView+Category
-#import "UITextView+IndicateWordLimit.h"
-#import "UITextView+Extend.h"
-#pragma mark —— NSObject
-#import "NSObject+Shake.h"
-#import "NSObject+DefConfig.h"
-#import "NSObject+DataSave.h"
-#import "NSObject+CallBackInfoByBlock.h"
-#pragma mark —— UIImageView
-// BaseImageView
-#import "BaseImageView.h"
-// UIImageView+Category
-#import "UIImageView+GIF.h"//UIImageView支持GIF动画 https://github.com/pupboss/UIImageView-GIF
+#import "JobsBaseCustomizeUIKitCoreHeader.h"
 
 #pragma mark —— Manual_Add_ThirdParty_h
 #import "UserDefaultManager.h"
